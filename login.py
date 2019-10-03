c11761ead9a42bdd50c2963a9750cbd709de6e19
jonah <jonah.n.rubin@gmail.com>
2019-10-02 23:13:28 -0400

    cleanup

--- login.py
+++ login.py
@@ -14,54 +14,37 @@ def get_name_pass(url):
         login_file.close()
         return USERNAME, PASSWORD
 
-
     except:
         return "NULL", "NULL"
-
 
 # set up get request
 def start_session(LOGIN_URL, URL, USERNAME, PASSWORD):
 
-    # with requests.session() as sess:
+    with requests.session() as sess:
 
-    s = requests.session()
+        result = sess.get(LOGIN_URL)
 
-    # get hidden tokens
-    result = s.get(LOGIN_URL)
-    tree = html.fromstring(result.text)
-    authenticity_token_1 = list(set(tree.xpath("//input[@name='RT']/@value")))[0]
-    authenticity_token_2 = list(set(tree.xpath("//input[@name='ST']/@value")))[0]
+        # login attempt purely to scrape secret tokens
+        login_result = sess.get(LOGIN_URL, headers=dict(referer=LOGIN_URL))
+        LOGIN_URL = login_result.request.url
 
-    # set up payload
-    payload = {
-        "RT": authenticity_token_1,
-        "ST": authenticity_token_2,
-        "login": "yes",
-        "username": USERNAME,
-        "password": PASSWORD
-    }
+        # parse secret tokens
+        str = (login_result.request.url.split("RT=")[1])
+        hidden_keys = str.split(";ST=")
 
+        # setup payload
+        payload = {
+            "RT": hidden_keys[0],
+            "ST": hidden_keys[1],
+            "login": "yes",
+            "username": USERNAME,
+            "password": PASSWORD
+        }
 
-    # login attempt
-    login_result = s.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
-
-    LOGIN_URL = login_result.request.url
-
-    str = (login_result.request.url.split("RT=")[1])
-    hidden_keys = str.split(";ST=")
-    payload = {
-        "RT": hidden_keys[0],
-        "ST": hidden_keys[1],
-        "login": "yes",
-        "username": USERNAME,
-        "password": PASSWORD
-    }
-
-    login_result = s.post(login_result.request.url, data=payload, headers=dict(referer=LOGIN_URL))
-
+        # actual login attempt
+        login_result = sess.post(login_result.request.url, data=payload, headers=dict(referer=LOGIN_URL))
 
 def main():
-    # constants
     LOGIN_TEXT = "login.txt"
     USERNAME, PASSWORD = get_name_pass(LOGIN_TEXT)
     LOGIN_URL = "https://myuvm.uvm.edu"
