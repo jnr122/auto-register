faf63a96bc90f8b6cd26d3cdf20e32636294d7d3
jonah <jonah.n.rubin@gmail.com>
2019-11-13 22:49:22 -0500

    no longer hardcoded

--- auto_login.py
+++ auto_login.py
@@ -1,23 +1,14 @@
 import requests
+import constants
 from lxml import html
-
-# globals
-
-# nav links
-AIS_LOGIN_URL = "https://aisweb1.uvm.edu/pls/owa_prod/twbkwbis.P_ValLogin"
-AIS_MENU_URL = "https://aisweb1.uvm.edu/pls/owa_prod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu"
-AIS_TERM_SELECTION_URL = "https://aisweb1.uvm.edu/pls/owa_prod/bwskfreg.P_AltPin"
-ADD_URL = "https://aisweb1.uvm.edu/pls/owa_prod/bwckcoms.P_Regs"
-TERM = "202001"
-DUMMY = "RSTS_IN=DUMMY&assoc_term_in=DUMMY&CRN_IN=DUMMY&start_date_in=DUMMY&end_date_in=DUMMY&SUBJ=DUMMY&CRSE=DUMMY&SEC=DUMMY&LEVL=DUMMY&CRED=DUMMY&GMOD=DUMMY&TITLE=DUMMY&MESG=DUMMY&REG_BTN=DUMMY&UVM_REQ_WD_IND_IN=DUMMY&MESG=DUMMY&"
-MYUVM_LOGIN_URL = "https://myuvm.uvm.edu"
-
-# so I look like a browser
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
 
 # user info
 LOGIN_TEXT = "login.txt"
+CLASSES_TEXT = "classes.txt"
 USERNAME, PASSWORD = "NULL", "NULL"
+
+CLASSES = []
+TERM = ""
 
 def get(sess, url, headers):
     return sess.get(url, headers=headers)
@@ -25,28 +16,30 @@ def get(sess, url, headers):
 def post(sess, url, payload, headers):
     return sess.post(url, payload, headers=headers)
 
-# get password from textfile
-def get_name_pass(url):
+# get data from textfile
+def read_file(url):
+    entries = []
     try:
-        login_file = open(url, "r")
-        login = login_file.readline().split()
-        USERNAME = login[0]
-        PASSWORD = login[1]
+        file = open(url, "r")
 
-        login_file.close()
-        return USERNAME, PASSWORD
+        for line in file:
+            entries.append(line.rstrip())
+
+        file.close()
+        return entries
 
     except:
         print("Place username and password in adjacent login.txt file in format: username password")
         exit()
 
+
 # perform login with hidden tokens for myuvm page
 # no longer necessary, but maybe useful later
 def myuvm_login(sess, USERNAME, PASSWORD):
-    result = get(sess, MYUVM_LOGIN_URL, dict(referer=MYUVM_LOGIN_URL))
+    result = get(sess, constants.MYUVM_LOGIN_URL, dict(referer=constants.MYUVM_LOGIN_URL))
 
     # login attempt purely to scrape secret tokens
-    login_result = get(sess, MYUVM_LOGIN_URL, dict(referer=MYUVM_LOGIN_URL))
+    login_result = get(sess, constants.MYUVM_LOGIN_URL, dict(referer=constants.MYUVM_LOGIN_URL))
 
     # parse secret tokens
     str = (login_result.request.url.split("RT=")[1])
@@ -62,7 +55,7 @@ def myuvm_login(sess, USERNAME, PASSWORD):
     }
 
     # actual login attempt
-    login_result = post(sess, login_result.request.url, login_payload, dict(referer=MYUVM_LOGIN_URL))
+    login_result = post(sess, login_result.request.url, login_payload, dict(referer=constants.MYUVM_LOGIN_URL))
 
     return login_result
 
@@ -77,16 +70,19 @@ def aisuvm_login(sess, USERNAME, PASSWORD):
 
     # update header dict with accepted submission format
     # perform login, then get menu
-    result = post(sess, AIS_LOGIN_URL, login_payload, {'referer': AIS_LOGIN_URL, 'user-agent': USER_AGENT })
-    result = post(sess, AIS_LOGIN_URL, login_payload, {'referer': AIS_LOGIN_URL, 'user-agent': USER_AGENT})
-    result = get(sess, AIS_MENU_URL, {'referer' : AIS_MENU_URL, 'user-agent' : USER_AGENT})
+    result = post(sess, constants.AIS_LOGIN_URL, login_payload,
+                  {'referer': constants.AIS_LOGIN_URL, 'user-agent': constants.USER_AGENT })
+    result = post(sess, constants.AIS_LOGIN_URL, login_payload,
+                  {'referer': constants.AIS_LOGIN_URL, 'user-agent': constants.USER_AGENT})
+    result = get(sess, constants.AIS_MENU_URL,
+                 {'referer' : constants.AIS_MENU_URL, 'user-agent' : constants.USER_AGENT})
 
     return result
 
 # scrape existing CRNS before passing in new ones
 def make_add_payload(result):
     s = "term_in=" + TERM + "&"
-    s += DUMMY
+    s += constants.DUMMY
 
     tree = html.fromstring(result.text)
     crns = list(set(tree.xpath("//input[@name='CRN_IN']/@value")))
@@ -95,6 +91,8 @@ def make_add_payload(result):
     for crn in crns:
         s += "RSTS_IN=&UVM_REQ_WD_IND_IN=" + crn + "+N&assoc_term_in=" + TERM + "&CRN_IN=" + crn + "&"
 
+    # for i in range(len(CLASSES)):
+    #     s += "RSTS_IN=RW&CRN_IN="+CLASSES[i]+"&assoc_term_in=&start_date_in=&end_date_in=&UVM_REQ_WD_IND_IN=DUMMY&"
     s += "RSTS_IN=RW&CRN_IN=10637&assoc_term_in=&start_date_in=&end_date_in=&UVM_REQ_WD_IND_IN=DUMMY&" #latin
     s += "RSTS_IN=RW&CRN_IN=10385&assoc_term_in=&start_date_in=&end_date_in=&UVM_REQ_WD_IND_IN=DUMMY&" #bb
     s += "RSTS_IN=RW&CRN_IN=10670&assoc_term_in=&start_date_in=&end_date_in=&UVM_REQ_WD_IND_IN=DUMMY&" #astr
@@ -109,17 +107,24 @@ def start_session(USERNAME, PASSWORD):
         login_result = aisuvm_login(sess, USERNAME, PASSWORD)
 
         # TODO: un-hardcode term selection
-        result = post(sess, AIS_TERM_SELECTION_URL, {"term_in" : TERM}, {'referer': AIS_TERM_SELECTION_URL, 'user-agent': USER_AGENT})
+        result = post(sess, constants.AIS_TERM_SELECTION_URL, {"term_in" : TERM},
+                      {'referer': constants.AIS_TERM_SELECTION_URL, 'user-agent':  constants.USER_AGENT})
         add_payload = make_add_payload(result)
 
-        result = post(sess, ADD_URL, add_payload, {'referer': AIS_TERM_SELECTION_URL, 'user-agent': USER_AGENT})
-
-
+        result = post(sess, constants.ADD_URL, add_payload,
+                      {'referer':  constants.AIS_TERM_SELECTION_URL, 'user-agent':  constants.USER_AGENT})
 
 def main():
-    global USER_NAME, PASSWORD
+    global USERNAME, PASSWORD, TERM, CLASSES
 
-    USERNAME, PASSWORD = get_name_pass(LOGIN_TEXT)
+    entries = read_file(LOGIN_TEXT)
+    USERNAME = entries[0]
+    PASSWORD = entries[1]
+
+    entries = read_file(CLASSES_TEXT)
+    TERM = entries[0]
+    CLASSES = entries[1:]
+
     start_session(USERNAME, PASSWORD)
 
 if __name__ == '__main__':
---
+++ constants.py
@@ -0,0 +1,11 @@
+
+# nav links
+AIS_LOGIN_URL = "https://aisweb1.uvm.edu/pls/owa_prod/twbkwbis.P_ValLogin"
+AIS_MENU_URL = "https://aisweb1.uvm.edu/pls/owa_prod/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu"
+AIS_TERM_SELECTION_URL = "https://aisweb1.uvm.edu/pls/owa_prod/bwskfreg.P_AltPin"
+ADD_URL = "https://aisweb1.uvm.edu/pls/owa_prod/bwckcoms.P_Regs"
+DUMMY = "RSTS_IN=DUMMY&assoc_term_in=DUMMY&CRN_IN=DUMMY&start_date_in=DUMMY&end_date_in=DUMMY&SUBJ=DUMMY&CRSE=DUMMY&SEC=DUMMY&LEVL=DUMMY&CRED=DUMMY&GMOD=DUMMY&TITLE=DUMMY&MESG=DUMMY&REG_BTN=DUMMY&UVM_REQ_WD_IND_IN=DUMMY&MESG=DUMMY&"
+MYUVM_LOGIN_URL = "https://myuvm.uvm.edu"
+
+# so I look like a browser
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
